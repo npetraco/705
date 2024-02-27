eeb14c0a6daab55e37f3482a0e108a0345bb08e6
npetraco <npetraco@gmail.com>
2024-02-26 22:00:26 -0500

    update

--- R/script_bucket/qqplot_ex.R
+++ R/script_bucket/qqplot_ex.R
@@ -15,3 +15,11 @@ qqline(z)
 ref.samp <- rnorm(length(x), mean = 0, sd = 1)                    # Get a sample from a reference distribution.
 qqplot(ref.samp, z, main="QQ-Plot", xlim=c(-3,3), ylim=c(-3,3))   # qqplot() orders and plots both the reference and the empirical sample against each other
 qqline(z, distribution = function(p){qnorm(p, mean = 0, sd = 1)})
+
+# This version gives the same result as qqnorm() followed by qqline()
+z.norm <- qnorm( ppoints(z), mean = 0, sd = 1)
+qqplot(z.norm, z, main="QQ-Plot", xlim=c(-3,3), ylim=c(-3,3))   # qqplot() orders and plots both the reference and the empirical sample against each other
+qqline(z, distribution = function(p){qnorm(p, mean = 0, sd = 1)})
+
+# This is a QQ-plot between two arbitrary samples
+plot(sort(ref.samp), sort(z))
